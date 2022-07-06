99f8783a607a507eaac1b778b63fda5e1ee06b86
paulcpro <paul.creuzeau@gmail.com>
2022-07-06 22:55:44 +0200

    Landing-page : simple modification of UI to show some text

--- client/src/app/landing-page/components/landing-page-component/landing-page/landing-page-component.component.ts
+++ client/src/app/landing-page/components/landing-page-component/landing-page/landing-page-component.component.ts
@@ -17,7 +17,7 @@ export class LandingPageComponent implements OnInit {
   }
 
   onContinue(): void {
-    this.router.navigateByUrl('facesnaps');
+    this.router.navigateByUrl('projettiles');
   }
 
   onSubmitForm(form: NgForm) {
